303d6409f16c060b43a50d08ebd6930f47d7123e
Sam Macpherson <sam.macpherson15@gmail.com>
2020-11-26 13:35:12 -0500

    Change DJANGO_SECRET to be a testing key.

--- subletshark/settings.py
+++ subletshark/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd4ickwkos#tal(5$(up#0zqbh)xd8=na7w+jl8+(zkiol7x6g8'
+SECRET_KEY = 'absolutely-top-secret-do-not-use-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
